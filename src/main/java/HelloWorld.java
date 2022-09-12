5f3d24bea69fc28b9937746d1dcd9e715ce14777
Sarah Beckett <sarah.beckett@sky.uk>
2022-09-12 12:21:27 +0100

    Added White House exercise and new class for compound arithmetic demo

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,6 +6,17 @@ public class HelloWorld {
             System.out.println("I am a character: " + myChar);
         boolean myBool = true;
             System.out.println("I am a boolean: " + myBool);
+        int x = 100;
+        int y = 5;
+        int z = -30;
+        int result = x * y * z;
+        System.out.println(result);
+        String street = "1600 Pennsylvania Avenue NW";
+        String city = "Washington";
+        String state = "DC";
+        String zip = "20500";
+        String whiteHouseAddress = street +", " + city +", " + state + ", " + zip;
+        System.out.println(whiteHouseAddress);
         }
     }
 
