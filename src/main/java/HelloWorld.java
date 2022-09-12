727d29218f063a27d2916ba6d0531fc0f4727a32
Sarah Beckett <sarah.beckett@sky.uk>
2022-09-12 10:21:49 +0100

    Adding some chatty variables

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,12 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello third attempt to sync");
-        for (int i = 0; i < 10; i++) {
-            System.out.println("i = " + i);
+        int myInt = 7;
+            System.out.println("I am an integer: " + myInt);
+        char myChar = 's';
+            System.out.println("I am a character: " + myChar);
+        boolean myBool = true;
+            System.out.println("I am a boolean: " + myBool);
         }
     }
-    
-    private void sum(int a, int b, int c) {
-        int result = a + b + c;
-    }
-}
+
